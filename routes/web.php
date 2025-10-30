e724eab8d3f8c688a03f0d93371abddca3dbedb2
aokiji <rcm.webdev.pro@gmail.com>
2025-10-29 21:21:20 -0700

    create routes for each page

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\PhoenixController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome');
-})->name('home');
+Route::get('/', [WelcomeController::class, 'index'])->name('home');
+Route::get('/phoenix', [PhoenixController::class, 'index'])->name('phoenix');
