446fd344e220bdfe52c6bc0ea8384554587cb7d5
aokiji <rcm.webdev.pro@gmail.com>
2025-10-29 21:20:41 -0700

    create controllers for each page - their logic lives in the controller section

---
+++ app/Http/Controllers/PhoenixController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Inertia\Inertia;
+
+class PhoenixController extends Controller
+{
+    public function index()
+    {
+        return Inertia::render('Phoenix');
+    }
+}
