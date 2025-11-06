ed7f253f30a10ba011b5cd16152b70e2978d8e9b
aokiji <rcm.webdev.pro@gmail.com>
2025-11-05 16:35:54 -0800

    update item factory route

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,6 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', [WelcomeController::class, 'index'])->name('home');
-Route::get('/items', [ItemController::class, 'index'])->name('items');
+Route::resource('items', ItemController::class);
 Route::get('/inventory', [InventroyController::class, 'index'])->name('inventory');
 Route::get('/stock', [StockMovementController::class, 'index'])->name('stock');
